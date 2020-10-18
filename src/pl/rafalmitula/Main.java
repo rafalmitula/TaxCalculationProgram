400783a7d8757ba7f8ef80c05af840b8a2fed535
rafaluser <rafalmitula06@gmail.com>
2020-10-18 20:26:28 +0200

    init commit

---
+++ src/pl/rafalmitula/Main.java
@@ -0,0 +1,50 @@
+package pl.rafalmitula;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("Witamy w programie do rozliczania 23% VATu.");
+        System.out.println();
+
+        boolean shouldContinue = true;
+        Methods metody = new Methods();
+        Scanner scanner = new Scanner(System.in);
+
+        while (shouldContinue) {
+
+            System.out.println("--------------");
+            System.out.println("Wybierz opcję:");
+            System.out.println("1. Rozlicz VAT - kwota netto");
+            System.out.println("2. Rozlicz VAT - kwota brutto");
+            System.out.println("3. Wyjdź");
+            System.out.println("--------------");
+
+            int userChoice = 0;
+            try {
+
+                userChoice = scanner.nextInt();
+
+                switch (userChoice) {
+                    case 1 -> metody.calculateNetto();
+                    case 2 -> metody.calculateBrutto();
+                    case 3 -> shouldContinue = false;
+                    default -> System.out.println("Niepoprawny wybór!\nWybierz liczbę od 1 do 3.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Wprowadzono niepoprawne dane!\nSpróbuj jeszcze raz.");
+                scanner.next();
+
+            }
+
+        }
+
+    }
+
+}
+
---
+++ src/pl/rafalmitula/Methods.java
@@ -0,0 +1,54 @@
+package pl.rafalmitula;
+
+import java.util.Scanner;
+
+public class Methods {
+
+
+    public void calculateNetto() {
+        System.out.println("Podaj kwotę netto od jakiej chcesz obliczyć vat." + "\nW przypadku części dziesiętnych oddziel je przecinkiem.");
+
+
+        try {
+            Scanner scnNetto = new Scanner(System.in);
+            double amountCn = scnNetto.nextDouble();
+            double amountVatCn = amountCn * 0.23;
+            double amountBruttoCn = amountCn * 1.23;
+
+            System.out.println(String.format("Kwota netto: %.2f", amountCn));
+            System.out.println(String.format("Kwota podatku VAT: %.2f", amountVatCn));
+            System.out.println(String.format("Kwota brutto: %.2f", amountBruttoCn));
+        } catch (Exception e) {
+            System.out.println("Wprowadzone dane są niepoprawne!!" + "\nSpróbuj jeszcze raz.");
+            System.out.println();
+            calculateNetto();
+        }
+
+
+    }
+
+    public void calculateBrutto() {
+        System.out.println("Podaj kwotę brutto od jakiej chcesz obliczyć VAT." + "\nW przypadku części dziesiętnych oddziel je przecinkiem.");
+
+        try {
+            Scanner scnBrutto = new Scanner(System.in);
+            double amountCb = scnBrutto.nextDouble();
+
+            double amountVatCb = (amountCb * 0.23) / (1.23);
+            double amountNettoCb = (amountCb / (123)) * 100;
+
+            System.out.println(String.format("Kwota netto: %.2f", amountNettoCb));
+            System.out.println(String.format("Kwota podatku VAT: %.2f", amountVatCb));
+            System.out.println(String.format("Kwota brutto: %.2f", amountCb));
+        } catch (Exception e) {
+            System.out.println("Wprowadzone dane są niepoprawne!" + "\nSpróbuj jeszcze raz.");
+            System.out.println();
+            calculateBrutto();
+        }
+
+    }
+
+
+}
+
+
