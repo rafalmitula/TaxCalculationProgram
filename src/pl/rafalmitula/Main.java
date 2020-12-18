9c6e52eb9b373c17fd8a6df6c3119bd85bbf6cd8
rafaluser <rafalmitula06@gmail.com>
2020-12-19 00:51:30 +0100

    Merge branch 'master' of C:\Users\rafaluser.000\IdeaProjects\ObiektoweRozliczanieVAT with conflicts.

--- src/pl/rafalmitula/Main.java
+++ src/pl/rafalmitula/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static boolean shouldContinue = true;
+    private static Methods methods = new Methods();
+
     public static void main(String[] args) {
 
         System.out.println();
         System.out.println("Witamy w programie do rozliczania 23% VATu.");
         System.out.println();
-
-        boolean shouldContinue = true;
-        Methods metody = new Methods();
-        Scanner scanner = new Scanner(System.in);
 
         while (shouldContinue) {
 
@@ -24,14 +24,13 @@ public class Main {
             System.out.println("3. Wyjdź");
             System.out.println("--------------");
 
-
             try {
 
                 int userChoice = scanner.nextInt();
 
                 switch (userChoice) {
-                    case 1 -> metody.calculateNetto();
-                    case 2 -> metody.calculateBrutto();
+                    case 1 -> methods.calculateNet();
+                    case 2 -> methods.calculateGross();
                     case 3 -> {
                         shouldContinue = false;
                         System.out.println("Dziękujęmy za skorzystanie z naszego programu.");
--- src/pl/rafalmitula/Methods.java
+++ src/pl/rafalmitula/Methods.java
@@ -5,46 +5,51 @@ import java.util.Scanner;
 public class Methods {
 
 
-    public void calculateNetto() {
+    public void calculateNet() {
         System.out.println("Podaj kwotę netto od jakiej chcesz obliczyć 23% VAT." +
                 "\nW przypadku części dziesiętnych oddziel je przecinkiem.");
 
-
         try {
-            Scanner scnNetto = new Scanner(System.in);
-            double amountCn = scnNetto.nextDouble();
+
+            Scanner scnNet = new Scanner(System.in);
+            double amountCn = scnNet.nextDouble();
             double amountVatCn = amountCn * 0.23;
-            double amountBruttoCn = amountCn * 1.23;
+            double amountGrossCn = amountCn * 1.23;
 
             System.out.println(String.format("Kwota netto: %.2f", amountCn));
             System.out.println(String.format("Kwota podatku VAT: %.2f", amountVatCn));
-            System.out.println(String.format("Kwota brutto: %.2f", amountBruttoCn));
+            System.out.println(String.format("Kwota brutto: %.2f", amountGrossCn));
+
         } catch (Exception e) {
-            System.out.println("Wprowadzone dane są niepoprawne!!" + "\nSpróbuj jeszcze raz.");
+
+            System.out.println("Wprowadzone dane są niepoprawne!!\nSpróbuj jeszcze raz.");
             System.out.println();
-            calculateNetto();
+            calculateNet();
         }
 
 
     }
 
-    public void calculateBrutto() {
+    public void calculateGross() {
         System.out.println("Podaj kwotę brutto od jakiej chcesz obliczyć 23% VAT." +
                 "\nW przypadku części dziesiętnych oddziel je przecinkiem.");
 
         try {
-            Scanner scnBrutto = new Scanner(System.in);
-            double amountCb = scnBrutto.nextDouble();
-            double amountVatCb = (amountCb * 0.23) / (1.23);
-            double amountNettoCb = (amountCb / (123)) * 100;
 
-            System.out.println(String.format("Kwota netto: %.2f", amountNettoCb));
-            System.out.println(String.format("Kwota podatku VAT: %.2f", amountVatCb));
-            System.out.println(String.format("Kwota brutto: %.2f", amountCb));
+            Scanner scnGross = new Scanner(System.in);
+            double amountCg = scnGross.nextDouble();
+            double amountVatCg = (amountCg * 0.23) / (1.23);
+            double amountNetCg = (amountCg / (123)) * 100;
+
+            System.out.println(String.format("Kwota netto: %.2f", amountNetCg));
+            System.out.println(String.format("Kwota podatku VAT: %.2f", amountVatCg));
+            System.out.println(String.format("Kwota brutto: %.2f", amountCg));
+
         } catch (Exception e) {
-            System.out.println("Wprowadzone dane są niepoprawne!" + "\nSpróbuj jeszcze raz.");
+
+            System.out.println("Wprowadzone dane są niepoprawne!\nSpróbuj jeszcze raz.");
             System.out.println();
-            calculateBrutto();
+            calculateGross();
         }
 
     }
